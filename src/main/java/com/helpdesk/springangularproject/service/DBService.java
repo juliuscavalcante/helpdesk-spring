6d8955c65c031969822188e5714cb7253aaa4e97
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-20 21:21:58 -0300

    populating database

--- src/main/java/com/helpdesk/springangularproject/service/DBService.java
+++ src/main/java/com/helpdesk/springangularproject/service/DBService.java
@@ -28,17 +28,29 @@ public class DBService {
 
     public void instantiateDB() {
         Technician tec1 = new Technician(null, "Galandriel", "806.096.260-43", "galandriel@email.com", "123");
+        Technician tec2 = new Technician(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", "123");
+        Technician tec3 = new Technician(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", "123");
+        Technician tec4 = new Technician(null, "Tim Berners-Lee", "162.720.120-39", "lee@mail.com", "123");
+        Technician tec5 = new Technician(null, "Linus Torvalds", "778.556.170-27", "linus@mail.com", "123");
         tec1.addProfile(Profile.ADMIN);
 
         Client cli1 = new Client(null, "Elrond", "316.119.620-13", "elrond@email.com", "123");
+        Client cli2 = new Client(null, "Marie Curie", "322.429.140-06", "curie@mail.com", "123");
+        Client cli3 = new Client(null, "Charles Darwin", "792.043.830-62", "darwin@mail.com", "123");
+        Client cli4 = new Client(null, "Stephen Hawking", "177.409.680-30", "hawking@mail.com", "123");
+        Client cli5 = new Client(null, "Max Planck", "081.399.300-83", "planck@mail.com", "123");
         cli1.addProfile(Profile.CLIENT);
 
         Request req1 = new Request(null, Priority.MEDIUM, Status.PROGRESS, "Request 01", "Test request", tec1, cli1);
+        Request req2 = new Request(null, Priority.HIGH, Status.OPEN, "Chamado 2", "Teste chamado 2", tec1, cli2);
+        Request req3 = new Request(null, Priority.LOW, Status.CLOSED, "Chamado 3", "Teste chamado 3", tec2, cli3);
+        Request req4 = new Request(null, Priority.HIGH, Status.OPEN, "Chamado 4", "Teste chamado 4", tec3, cli3);
+        Request req5 = new Request(null, Priority.MEDIUM, Status.PROGRESS, "Chamado 5", "Teste chamado 5", tec2, cli1);
 
-        technicianRepository.saveAll(Arrays.asList(tec1));
+        technicianRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5));
 
-        clientRepository.saveAll(Arrays.asList(cli1));
+        clientRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5));
 
-        requestRepository.saveAll(Arrays.asList(req1));
+        requestRepository.saveAll(Arrays.asList(req1, req2, req3, req4, req5));
     }
 }
