faf0eb77e62533a761e1e791e33adee5520aeee7
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-28 12:32:15 -0300

    creating CustomerDTO class

---
+++ src/main/java/com/helpdesk/springangularproject/domain/dto/CustomerDTO.java
@@ -0,0 +1,103 @@
+package com.helpdesk.springangularproject.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.helpdesk.springangularproject.domain.Customer;
+import com.helpdesk.springangularproject.domain.enums.Profile;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomerDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 972259293075289989L;
+
+    protected Long id;
+    @NotBlank(message = "name is required")
+    protected String name;
+    @NotBlank(message = "CPF is required")
+    protected String cpf;
+    @NotBlank(message = "email is required")
+    protected String email;
+    @NotBlank(message = "password is required")
+    protected String password;
+    protected Set<Integer> profiles = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate creationDate = LocalDate.now();
+
+    public CustomerDTO(Customer customer) {
+        this.id = customer.getId();
+        this.name = customer.getName();
+        this.cpf = customer.getCpf();
+        this.email = customer.getEmail();
+        this.password = customer.getPassword();
+        this.profiles = customer.getProfiles().stream().map(Profile::getCode).collect(Collectors.toSet());
+        this.creationDate = customer.getCreationDate();
+    }
+
+    public CustomerDTO() {
+        super();
+        addProfile(Profile.CUSTOMER);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Profile> getProfiles() {
+        return profiles.stream().map(Profile::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addProfile(Profile profile) {
+        this.profiles.add(profile.getCode());
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+}
