70791fdb9b624af53874a6de4cbbec3848990e71
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-29 11:01:45 -0300

    feat: implements findAll endpoint for Request

--- src/main/java/com/helpdesk/springangularproject/controller/RequestController.java
+++ src/main/java/com/helpdesk/springangularproject/controller/RequestController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/requests")
 public class RequestController {
@@ -21,5 +24,13 @@ public class RequestController {
     public ResponseEntity<RequestDTO> findById(@PathVariable Long id) {
         Request request = requestService.findById(id);
         return ResponseEntity.ok().body(new RequestDTO(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RequestDTO>> findAll() {
+        List<Request> requestList = requestService.findAll();
+        List<RequestDTO> requestDTOList = requestList.stream()
+                .map(request -> new RequestDTO(request)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(requestDTOList);
     }
 }
--- src/main/java/com/helpdesk/springangularproject/service/RequestService.java
+++ src/main/java/com/helpdesk/springangularproject/service/RequestService.java
@@ -6,6 +6,7 @@ import com.helpdesk.springangularproject.service.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,9 @@ public class RequestService {
     public Request findById(Long id) {
         Optional<Request> requestOptional = requestRepository.findById(id);
         return requestOptional.orElseThrow(() -> new ObjectNotFoundException("Request id " + id + " not found"));
+    }
 
+    public List<Request> findAll() {
+        return requestRepository.findAll();
     }
 }
